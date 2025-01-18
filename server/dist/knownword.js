22b69063e73216366b7abadab7346f9226750df2
Ruth Kurniawati <ruth.kurniawati@gmail.com>
2025-01-18 15:44:31 -0500

    exclude generated files

--- server/dist/knownword.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class KnownWord {
-    constructor(word, locationMarkers) {
-        this.word = word;
-        this.locationMarkers = locationMarkers;
-    }
-}
-exports.default = KnownWord;
